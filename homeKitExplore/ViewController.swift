4e84d86db10c7258a328cf4a445bb9db6864069b
Spiros Raptis <spirosrap@gmail.com>
2015-04-29 20:37:58 +0300

    adding a home

--- homeKitExplore/ViewController.swift
+++ homeKitExplore/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import HomeKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,HMHomeManagerDelegate {
+    var homeManager = HMHomeManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.homeManager.delegate = self
+        
+        self.homeManager.addHomeWithName("SpirosHome"){
+            home,error in
+            if error != nil{
+                println(error)
+            }else{
+                println(home)
+                println(self.homeManager.homes)
+                println("Succesfully added a home")
+            }
+        }
+        
+        println(self.homeManager.homes)
+
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func homeManagerDidUpdateHomes(manager: HMHomeManager) {
+        
+    }
 
 }
 
